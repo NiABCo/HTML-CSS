061de5d690b0c831e5aa5f0923c8e723fca04e4d
NiAbCo <nico.abbadie@gmail.com>
2019-12-17 11:10:49 +0100

    first commit

---
+++ PostCss.config.js
@@ -0,0 +1,1 @@
+if (process.env.NODE_ENV === 'production') {   module.exports = {    plugins: [    require('autoprefixer'),    require('cssnano'),    // More postCSS modules here if needed    ]    }   }
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+/**
+ * Main
+ * @author Nico
+ * @version 1.0.0
+ * Entry point of our app
+ */
+export class Main{
+
+    public constructor(){
+
+        console.log('Hello Typescript!');
+    }
+}
+
+//Boostraping of our app
+const app: Main = new Main();
